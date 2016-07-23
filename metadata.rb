618e5a86d5c7046514ad6401d58bb60d29c3ce4d
Ravi Banamigi <ravibanamigi@gmail.com>
2016-07-23 07:50:02 +0530

    updated  pptpd

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'pptpd'
+maintainer       'Ravi Banamigi'
+maintainer_email 'ravibanamigi@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures pptpd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/pptp_box1.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: pptpd
+# Recipe:: default
+#
+# Copyright 2010, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# 
+
+package "pptpd" do
+  action :install
+end
+execute "adding kernel modules" do
+	command "modprobe ppp_mppe"
+end
+
+
+template "/etc/ppp/peers/pptpserver" do
+  source "pptpserver.erb"
+  owner "root"
+  group "root"
+  mode "0664"
+end
+pppd call pptpserver
+
+execute "calling pptpserver" do
+	command "pppd call pptpserver"
+end
+
+execute "adding route to pptpserver" do
+	command "ip route add 10.0.0.0/8 dev ppp0"
+end
